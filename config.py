d64e7d6a323a5899ef1abf659f46398f601cc033
hmb1998 <arekani341@gmail.com>
2026-08-20 18:26:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,4 @@
 import os
 
-# Bot token - read ONLY from environment variable
-# NEVER hardcode this in any file!
-TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
-
-# GitHub files URL (optional - can be set via fly secrets too)
-GITHUB_FILES_URL = os.environ.get('GITHUB_FILES_URL', '')
-
-# Bot settings
-BOT_PREFIX = '!'
-DEFAULT_VOLUME = 0.5  # 0.0 to 1.0
-MAX_QUEUE_SIZE = 50
+TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
+DEFAULT_VOLUME = 0.5
